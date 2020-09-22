5049e09ac767ddcf6fbcd4496ab85e0064bfef12
hanan7121993 <hanan7121993@gmail.com>
2020-09-22 08:05:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,61 +1,39 @@
 // Add your code here
-let formData = {
-  "firstName": "Eli",
-  "registryMessage": "Howdy doody"
-}
-function registerSelf() {
-  return fetch('http://guestbook.example.com/register', configObject)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(json) {
-    return json.message;
-  })
-}
+function submitData(name, email) {   
+    let formData = {
+        name: name,
+        email: email
+      };
 
-function errorSelf() {
-  return fetch('http://guestbook.example.com/register-error', configObject)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(json) {
-    return json.message;
-  })
-  .catch(function(error) {
-    return ":("
-  });
-let configObject = {
-  method: "POST",
-  body: formData
-}
-function submitData(name, email) {
-	let formData = {
-		name: name,
-		email: email
-	};
+      // method: "POST" is missing from the object below
+      let configObj = {
+        method: "POST",          
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+      };
 
-	let configurationObj = {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-			"Accept": "application/json"
-		},
-		body: JSON.stringify(formData)
-	};
+      return fetch("http://localhost:3000/users", configObj)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(object) {
+          const body = document.getElementsByTagName('body');  
+          var h1 = document.createElement('H1');
+          h1.innerHTML = object.id;
+          body[0].appendChild(h1);          
+          console.log(object.id);
+        })
+        .catch(function(error) {
+          alert("Bad things! Ragnarők!");
+          const body = document.getElementsByTagName('body');  
+          var h2 = document.createElement('H2');
+          h2.innerHTML = error.message;
+          body[0].appendChild(h2);                    
+          console.log(error.message);
+        });
+}  
 
-	return fetch("http://localhost:3000/users", configurationObj)
-	.then( (response) => {
-		return response.json()
-	})
-	.then( (object) => {
-		let h2 = document.createElement('h2');
-		h2.innerHTML = object.id;
-		document.body.appendChild(h2);
-		console.log(object);
-	})
-	.catch( (error) => {
-		let h3 = document.createElement('h3');
-		h3.innerHTML = error.message;
-		document.body.appendChild(h3);
-		console.log(error.message);
-	});
+submitData("Elliot","public@elliotblanchard.com");
