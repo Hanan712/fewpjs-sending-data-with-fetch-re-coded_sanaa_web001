1507ae5dab60451d3f555e852d3abb27453aa617
hanan7121993 <hanan7121993@gmail.com>
2020-09-22 08:05:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Add your code here
+function submitData(name, email) {
+    let formData = {
+        name: name,
+        email: email
+      };
+
+      // method: "POST" is missing from the object below
+      let configObj = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(formData)
+      };
+
+      return fetch("http://localhost:3000/users", configObj)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(object) {
+          const body = document.getElementsByTagName('body');
+          var h1 = document.createElement('H1');
+          h1.innerHTML = object.id;
+          body[0].appendChild(h1);
+          console.log(object.id);
+        })
+        .catch(function(error) {
+          alert("Bad things! Ragnarők!");
+          const body = document.getElementsByTagName('body');
+          var h2 = document.createElement('H2');
+          h2.innerHTML = error.message;
+          body[0].appendChild(h2);
+          console.log(error.message);
+        });
+}
+
+submitData("Elliot","public@elliotblanchard.com");
